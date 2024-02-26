7b034999e261eed7e54d9a888ebcd0ca455890b4
Afrin Firoj Shaikh <afrinfs@maveric-systems.com>
2024-02-26 15:32:51 +0530

    Hello

---
+++ src/test/java/com/example/afrin/AfrinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.afrin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AfrinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
